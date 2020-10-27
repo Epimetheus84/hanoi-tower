0174f02883b9b75cd0c43ca1a7fa869bcdaf7239
Dmitry Shorokhov <vip.shoroch@gmail.com>
2020-10-28 02:21:35 +0300

    solve first version

--- puzzle.py
+++ puzzle.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import re
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 
 # (disk_index, source_pin_number, destination_pin_number)
 Step = Tuple[int, int, int]
@@ -13,26 +13,40 @@ class HanoiTowerPuzzleException(RuntimeError):
 
 
 class HanoiTowerPuzzle:
-    def __init__(self, disks_number: int, costs: List[int], test_decision: Decision = None):
-        self.pins = []
-        for pin_index in range(len(costs)):
-            self.pins.append([])
-        self.pins[0] = [(disks_number - 1 - disk_index) for disk_index in range(disks_number)]
+    def __init__(self, disks_number: int, costs: List[int], check_decision: Decision = None, src_peg_index: int = 0):
+        self.pegs = []
+        for peg_index in range(len(costs)):
+            self.pegs.append([])
+        self.pegs[0] = [(disks_number - 1 - disk_index) for disk_index in range(disks_number)]
         self.costs = costs
-        self.test_decision = test_decision
+        self.check_decision = check_decision
+        self.decision = []
+        self.src_peg_index = src_peg_index
 
-    def solve(self) -> Decision:
-        pass
+    def solve(self, dst_peg_index: int = None) -> Optional[Decision]:
+        if not dst_peg_index:
+            dst_peg_index = self._get_optimal_peg([self.src_peg_index])
+        disks_number = len(self.pegs[self.src_peg_index])
+        pegs_number = len(self.pegs)
+        if pegs_number == 2 and disks_number > 1:
+            return None
+        if pegs_number == 4:
+            free_peg_index_1 = self._get_optimal_peg([self.src_peg_index, dst_peg_index])
+            free_peg_index_2 = self._get_optimal_peg([self.src_peg_index, dst_peg_index, free_peg_index_1])
+            self._move_stack_reve(disks_number, self.src_peg_index, dst_peg_index, free_peg_index_1, free_peg_index_2)
+        else:
+            self._move_stack(disks_number, self.src_peg_index, dst_peg_index)
+        return self.decision
 
     def calc_decision_cost(self, decision: Decision) -> int:
         cost = 0
-        for (_, _, dest_pin_number) in decision:
-            cost += dest_pin_number * self.costs[dest_pin_number]
+        for (_, _, dest_peg_number) in decision:
+            cost += self.costs[dest_peg_number]
         return cost
 
     @staticmethod
     def load_from_file(path: str) -> HanoiTowerPuzzle:
-        pins_number = None
+        pegs_number = None
         costs = []
         orders = []
         with open(path, 'r') as input_file:
@@ -49,7 +63,7 @@ class HanoiTowerPuzzle:
             for block in blocks:
                 block_split = block.split(' ')
                 if block.startswith('INIT'):
-                    _, key, pins_number = block_split
+                    _, key, pegs_number = block_split
                     if key != 'NPARTS':
                         raise HanoiTowerPuzzleException('Incorrect input data')
                 elif block.startswith('COST'):
@@ -58,13 +72,60 @@ class HanoiTowerPuzzle:
                 elif block.startswith('ORDER'):
                     order_content = block_split[1:]
                     orders = [
-                        list(map(lambda x: int(x), order_content[i:i + 3]))
+                        tuple(map(lambda x: int(x), order_content[i:i + 3]))
                         for i in range(0, len(order_content), 3)
                     ]
                 else:
-                    key, pins_number = block_split
+                    key, pegs_number = block_split
                     if key != 'NPARTS':
                         raise HanoiTowerPuzzleException('Incorrect input data')
-        if not pins_number or not costs or not orders:
+        if not pegs_number or not costs or not orders:
             raise HanoiTowerPuzzleException('Incorrect input data')
-        return HanoiTowerPuzzle(int(pins_number), costs, orders)
+        return HanoiTowerPuzzle(int(pegs_number), costs, orders)
+
+    def _move_stack(self, stack_size: int, src_peg_index: int, dst_peg_index: int):
+        if stack_size == 0:
+            return
+        optimal_peg_index = self._get_optimal_peg([src_peg_index, dst_peg_index])
+        self._move_stack(stack_size - 1, src_peg_index, optimal_peg_index)
+        step = self._move_disk(src_peg_index, dst_peg_index)
+        self.decision.append(step)
+        self._move_stack(stack_size - 1, optimal_peg_index, dst_peg_index)
+
+    def _move_stack_reve(self, stack_size, src_peg_index, dst_peg_index, free_peg_index_1, free_peg_index_2):
+        if stack_size == 0:
+            return
+        if stack_size == 1:
+            step = self._move_disk(src_peg_index, dst_peg_index)
+            self.decision.append(step)
+            return
+        self._move_stack_reve(stack_size - 2, src_peg_index, free_peg_index_1, free_peg_index_2, dst_peg_index)
+
+        step = self._move_disk(src_peg_index, free_peg_index_2)
+        self.decision.append(step)
+        step = self._move_disk(src_peg_index, dst_peg_index)
+        self.decision.append(step)
+        step = self._move_disk(free_peg_index_2, dst_peg_index)
+        self.decision.append(step)
+
+        self._move_stack_reve(stack_size - 2, free_peg_index_1, dst_peg_index, src_peg_index, free_peg_index_2)
+
+    def _get_optimal_peg(self, exclude_indices: List[int] = None) -> int:
+        max_int = 2147483647
+        src_peg_top = max_int
+        optimal_peg_index = -1
+        optimal_peg_cost = max_int
+        for peg_index, peg_disks in enumerate(self.pegs):
+            if (exclude_indices is not None) and (peg_index in exclude_indices):
+                continue
+            peg_top = peg_disks[-1] if len(peg_disks) > 0 else -1
+            peg_cost = self.costs[peg_index]
+            if peg_top < src_peg_top and peg_cost < optimal_peg_cost:
+                optimal_peg_index = peg_index
+                optimal_peg_cost = peg_cost
+        return optimal_peg_index
+
+    def _move_disk(self, src_peg_index, dst_peg_index) -> Step:
+        disk = self.pegs[src_peg_index].pop()
+        self.pegs[dst_peg_index].append(disk)
+        return disk, src_peg_index, dst_peg_index
